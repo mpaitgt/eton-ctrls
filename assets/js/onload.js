a9f89d42886ba83905468df339605fb0642b364c
Matt Pignatore <mattpigs@gmail.com>
2020-04-30 19:20:38 -0400

    adding slideshow effect, but needs to be improved and not fade in / fade out

---
+++ assets/js/onload.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+  window.addEventListener('scroll', fadingNavBar);
+}
--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,21 +1,17 @@
-window.onload = function() {
+function fadingNavBar() {
   let
     opacity = 0,
     nav = document.querySelector('.main-nav'),
     bg = `rgb(41, 171, 226, ${opacity})`;
-    
+
+  opacity = (window.pageYOffset / 500);
+  bg = `rgb(41, 171, 226, ${opacity})`;
   nav.style.background = bg;
+  nav.style.zIndex = 999;
 
-  window.addEventListener('scroll', function() {
-    opacity = (window.pageYOffset / 500);
-    bg = `rgb(41, 171, 226, ${opacity})`;
-    nav.style.background = bg;
-    nav.style.zIndex = 999;
-
-    if (opacity >= 1) {
-      nav.style.boxShadow = '0px 0px 12px 0px rgba(0, 0, 0, 0.75)';
-    } else if (opacity <= 1) {
-      nav.style.boxShadow = 'none';
-    }
-  })
+  if (opacity >= 1) {
+    nav.style.boxShadow = '0px 0px 12px 0px rgba(0, 0, 0, 0.75)';
+  } else if (opacity <= 1) {
+    nav.style.boxShadow = 'none';
+  }
 }
