62710e7d037c80833d5c3515fbbe40abc438bfd3
Matt Pignatore <mattpigs@gmail.com>
2020-05-18 09:33:48 -0400

    changed out old images for new, fixed the burger navigation

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,13 +1,19 @@
 function fadingNavbar() {
   let
     opacity = 0,
-    nav = document.querySelector('.menu-content'),
+    nav = document.querySelector('.main-nav'),
     bg = `rgb(41, 171, 226, ${opacity})`;
 
-  opacity = (window.pageYOffset / 500);
-  bg = `rgb(41, 171, 226, ${opacity})`;
-  nav.style.background = bg;
-  nav.style.zIndex = 999;
+  if (window.innerWidth > 578) {
+    opacity = (window.pageYOffset / 500);
+    bg = `rgb(41, 171, 226, ${opacity})`;
+    nav.style.background = bg;
+    nav.style.zIndex = 999;
+  }
+
+  if (window.innerWidth <= 578) {
+    nav.style.background = `transparent`;
+  }
 
   if (opacity >= 1) {
     nav.style.boxShadow = '0px 0px 12px 0px rgba(0, 0, 0, 0.75)';
--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,18 +1,18 @@
-let slideIndex = 0;
-showSlides();
+// let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-  let 
-    slides = document.querySelectorAll('.bg-img');
+// function showSlides() {
+//   let 
+//     slides = document.querySelectorAll('.bg-img');
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
+//   for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.display = 'none';
+//   }
 
-  slideIndex++;
+//   slideIndex++;
 
-  if (slideIndex > slides.length) { slideIndex = 1 };
-  slides[slideIndex - 1].style.display = 'block';
-  setTimeout(showSlides, 5000);
-}
+//   if (slideIndex > slides.length) { slideIndex = 1 };
+//   slides[slideIndex - 1].style.display = 'block';
+//   setTimeout(showSlides, 5000);
+// }
 
